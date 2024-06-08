8d70318a951a2cf8514ef1debf11900ebd2a00fd
Jiří Kulík <jiri.kulik@jkulik.eu>
2024-06-08 13:40:13 +0000

    Creation of first page

---
+++ pages/1_Page_1.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.set_page_config(page_title='SWGOH Test')
+st.title('SWGOH Test')
+
+factions: list[str] = ['Jedi', 'Old Republic', 'Galactic Republic', 'Droid',
+                       'Rebel', 'Resistance']
+
+class Character:
+    """SWGOH Character Class"""
+    def __init__(self, name: str, factions_list: list[str]=[], leader: bool=False):
+        if name in [ch.name for ch in characters]:
+            raise ValueError(f'The character {name} already exists')
+        self.name = name
+        for f in factions_list:
+            if f not in factions:
+                raise ValueError(f'Faction {f} does not exist')
+        self.factions = factions_list
+        self.isLeader = leader
+
+characters: list[Character] = []
+
+ch: Character = Character(name='R2-D2',
+                          factions_list=['Droid', 'Rebel', 'Galactic Republic',
+                                         'Resistance'])
+characters.append(ch)
+st.write(ch.name)
+st.write(ch.factions)
